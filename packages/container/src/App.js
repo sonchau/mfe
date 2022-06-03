e61fe2d9eef52fdc592e0fb0d2940965fcd9473c
Son Chau <son.chau@pepperstone.com>
2022-06-04 08:04:25 +1000

    no longer need aws cli custom

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there! 123</h1>
+      <h1>Hi there! 1234</h1>
       <hr />
       <MarketingApp />
     </div>
